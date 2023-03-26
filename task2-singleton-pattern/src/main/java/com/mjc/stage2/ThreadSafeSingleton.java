a58cd493b9f460fd860a7c244ef3225b42d525df
SirJohanot <pavelkrafft@gmail.com>
2023-03-26 18:12:05 +0300

    completed the task

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -1,5 +1,29 @@
 package com.mjc.stage2;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ThreadSafeSingleton {
-    // Write your code here!
+    
+    private static ThreadSafeSingleton INSTANCE;
+
+    private static final ReentrantLock INSTANCE_LOCK = new ReentrantLock();
+
+    private ThreadSafeSingleton() {
+    }
+
+    public static ThreadSafeSingleton getInstance() {
+        ThreadSafeSingleton localInstance = INSTANCE;
+        if (localInstance == null) {
+            INSTANCE_LOCK.lock();
+            try {
+                localInstance = INSTANCE;
+                if (localInstance == null) {
+                    localInstance = INSTANCE = new ThreadSafeSingleton();
+                }
+            } finally {
+                INSTANCE_LOCK.unlock();
+            }
+        }
+        return localInstance;
+    }
 }
--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,48 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
-    // Write your code here!
-}
+import com.mjc.stage2.ConnectionFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class H2ConnectionFactory implements ConnectionFactory {
+
+    private static final String CONNECTION_PROPERTIES_FILE_NAME = "h2database.properties";
+    private static final String DRIVER_CLASS = "jdbc_driver";
+    private static final String CONNECTION_URL = "db_url";
+    private static final String USERNAME = "user";
+    private static final String PASSWORD = "password";
+
+    private final String databaseConnectionUrl;
+    private final String databaseUsername;
+    private final String databasePassword;
+
+    public H2ConnectionFactory() {
+        Properties properties = new Properties();
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(CONNECTION_PROPERTIES_FILE_NAME);
+        try {
+            properties.load(inputStream);
+            String databaseDriverClass = properties.getProperty(DRIVER_CLASS);
+            databaseConnectionUrl = properties.getProperty(CONNECTION_URL);
+            databaseUsername = properties.getProperty(USERNAME);
+            databasePassword = properties.getProperty(PASSWORD);
+            Class.forName(databaseDriverClass);
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Connection createConnection() {
+        try {
+            return DriverManager.getConnection(databaseConnectionUrl, databaseUsername, databasePassword);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
